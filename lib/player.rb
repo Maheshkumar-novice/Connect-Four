d632637cb10df3feb9d1ae3d8f537c4c3dcf8d46
Maheshkumar <callmeelprofessor@gmail.com>
2021-12-12 17:17:52 +0530

    Add player tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# class player
+class Player
+  attr_accessor :name, :marker
+
+  MAX_NAME_LENGTH = 15
+
+  def initialize
+    @name = nil
+    @marker = nil
+  end
+
+  def valid_name?(name)
+    name.match?(/^[\w+\s]{1,#{MAX_NAME_LENGTH}}$/)
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,3 +1,38 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative '../lib/player'
+
+describe Player do
+  subject(:player) { described_class.new }
+
+  describe '#valid_name?' do
+    context 'when a name with length 0 given' do
+      it 'returns false' do
+        name = ''
+        expect(player.valid_name?(name)).to be false
+      end
+    end
+
+    context 'when a name with length more than max length given' do
+      it 'returns false' do
+        name = 'qwertymanfrommarsgoingtovenus'
+        expect(player.valid_name?(name)).to be false
+      end
+    end
+
+    context 'when a name with special characters given' do
+      it 'returns false' do
+        name = '^hello $mate!'
+        expect(player.valid_name?(name)).to be false
+      end
+    end
+
+    context 'when a valid name given' do
+      it 'returns true' do
+        name = 'hello mate'
+        expect(player.valid_name?(name)).to be true
+      end
+    end
+  end
+end
