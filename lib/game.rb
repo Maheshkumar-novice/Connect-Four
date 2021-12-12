4235a094a7910b393a38b73c982848538f61ede9
Maheshkumar <callmeelprofessor@gmail.com>
2021-12-12 21:41:30 +0530

    Add test initialization for game

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative './board'
+require_relative './player'
+
+# class game
+class Game
+  def initialize
+    @player1 = Player.new
+    @player2 = Player.new
+    @board = Board.new
+    @current_player, @other_player = [@player1, @player2].shuffle
+    @markers = [':red', ':blue', ':green', ':yellow']
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative '../lib/game'
+
+describe Game do
+  subject(:game) { described_class.new }
+end
