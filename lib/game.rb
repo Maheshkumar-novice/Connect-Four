bd5353348fa4431367c60670d63e9f3be3ba9ebf
Maheshkumar <callmeelprofessor@gmail.com>
2021-12-13 16:16:25 +0530

    Add data methods

--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,36 @@ require_relative './player'
 
 # class game
 class Game
-  def initialize
-    @player1 = Player.new
-    @player2 = Player.new
-    @board = Board.new
+  def initialize(player1, player2, board)
+    @player1 = player1
+    @player2 = player2
+    @board = board
     @current_player, @other_player = [@player1, @player2].shuffle
     @markers = [':red', ':blue', ':green', ':yellow']
+  end
+
+  def update_player_data
+    update_player1_data
+    update_player2_data
+  end
+
+  def update_player1_data
+    @player1.name = 'hi'
+    @player1.marker = ':red'
+  end
+
+  def update_player2_data
+    @player2.name = 'hi'
+    @player2.marker = ':red'
+  end
+
+  def create_player_name
+    name = gets.chomp
+    until @player1.valid_name?(name)
+      puts 'Invalid Name!'
+      print '> '
+      name = gets.chomp
+    end
+    name
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,5 +4,92 @@
 require_relative '../lib/game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player1, player2, board) }
+  let(:player1) { instance_double(Player) }
+  let(:player2) { instance_double(Player) }
+  let(:board) { instance_double(Board) }
+
+  before do
+    game.instance_variable_set(:@current_player, player1)
+    game.instance_variable_set(:@other_player, player2)
+  end
+
+  describe '#update_player1_data' do
+    before do
+      allow(player1).to receive(:name=)
+      allow(player1).to receive(:marker=)
+    end
+
+    it 'sends message to player 1 to update name' do
+      expect(player1).to receive(:name=)
+      game.update_player1_data
+    end
+
+    it 'sends message to player 1 to update marker' do
+      expect(player1).to receive(:marker=)
+      game.update_player1_data
+    end
+  end
+
+  describe '#update_player2_data' do
+    before do
+      allow(player2).to receive(:name=)
+      allow(player2).to receive(:marker=)
+    end
+
+    it 'sends message to player 1 to update name' do
+      expect(player2).to receive(:name=)
+      game.update_player2_data
+    end
+
+    it 'sends message to player 1 to update marker' do
+      expect(player2).to receive(:marker=)
+      game.update_player2_data
+    end
+  end
+
+  describe '#create_player_name' do
+    before do
+      allow(game).to receive(:print).with('> ')
+    end
+
+    context 'when the invalid name given as input' do
+      context 'when invalid name given twice and a valid input given' do
+        before do
+          allow(game).to receive(:gets).and_return('', '', 'hell fury')
+          allow(player1).to receive(:valid_name?).and_return(false, false, true)
+        end
+
+        it 'shows error message twice' do
+          expect(game).to receive(:puts).with('Invalid Name!').twice
+          game.create_player_name
+        end
+
+        it 'returns the valid name' do
+          allow(game).to receive(:puts)
+          name = game.create_player_name
+          expect(name).to eq('hell fury')
+          game.create_player_name
+        end
+      end
+    end
+
+    context 'when a valid name given as input' do
+      before do
+        allow(game).to receive(:gets).and_return('hell fury')
+        allow(player1).to receive(:valid_name?).and_return(true)
+      end
+
+      it 'finishes execution without showing error message' do
+        expect(game).not_to receive(:puts).with('Invalid Name!')
+        game.create_player_name
+      end
+
+      it 'returns the valid name' do
+        name = game.create_player_name
+        expect(name).to eq('hell fury')
+        game.create_player_name
+      end
+    end
+  end
 end
