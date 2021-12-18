2bd74e23bc49b877c3bae6387dab0fac4e25c7af
Maheshkumar <callmeelprofessor@gmail.com>
2021-12-18 11:27:02 +0530

    Final Refactor: player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,28 +10,28 @@ describe Player do
     context 'when a name with length 0 given' do
       it 'returns false' do
         name = ''
-        expect(Player.valid_name?(name)).to be false
+        expect(Player).not_to be_valid_name(name)
       end
     end
 
     context 'when a name with length more than max length given' do
       it 'returns false' do
         name = 'qwertymanfrommarsgoingtovenus'
-        expect(Player.valid_name?(name)).to be false
+        expect(Player).not_to be_valid_name(name)
       end
     end
 
     context 'when a name with special characters given' do
       it 'returns false' do
         name = '^hello $mate!'
-        expect(Player.valid_name?(name)).to be false
+        expect(Player).not_to be_valid_name(name)
       end
     end
 
     context 'when a valid name given' do
       it 'returns true' do
         name = 'hello mate'
-        expect(Player.valid_name?(name)).to be true
+        expect(Player).to be_valid_name(name)
       end
     end
   end
